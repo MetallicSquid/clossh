0a400a19266ae3e72e5ca2d2c3fe6a7100a33e23
MetallicSquid <gcmacneil@icloud.com>
2021-02-18 18:36:00 +0000

    Commit for setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python
+
+from distutils.core import setup
+
+setup(name="clossh",
+      version="0.1.0",
+      description="A simple and lightweight clustering library based on the ssh protocol.",
+      author="Guillaume Macneil",
+      url="https://github.com/MetallicSquid/clossh",
+      packages=["control"])
